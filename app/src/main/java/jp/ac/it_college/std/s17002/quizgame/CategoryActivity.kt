7bca1ed0c7852d9f095570088d7e823db011285b
s17002 <s17002@std.it-college.ac.jp>
2018-12-06 09:34:05 +0900

    commit

---
+++ app/src/main/java/jp/ac/it_college/std/s17002/quizgame/CategoryActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.it_college.std.s17002.quizgame
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category2)
+    }
+}
